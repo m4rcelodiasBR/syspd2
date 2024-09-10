9d20634377eff203c8a414f1cecb0f137d74c631
Marcelo Dias <m4rcelodias@live.com>
2024-09-10 00:01:44 -0300

    Alterado os botões do menu e criado o menu de ajuda do sistema.

--- administracao.php
+++ administracao.php
@@ -38,10 +38,6 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
                             Menu
                             <span class="bi bi-menu-button-wide-fill"></span>
                         </button>
-                        <button class="btn btn-primary btn-sm px-3 btn-size-custom" type="button" data-bs-toggle="modal" data-bs-target="#modalSobre" aria-controls="modalSobre">
-                            Sobre
-                            <span class="bi bi-three-dots"></span>
-                        </button>
                     </div>
 
                     <div class="controles-data">
@@ -132,9 +128,9 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header bg-danger text-light">
                     <h5 class="modal-title" id="deleteModalLabel">Confirmar Exclusão</h5>
-                    <button type="button" class="btn-close btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn bg-light btn-close btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     Você tem certeza que deseja excluir este arquivo?
@@ -151,9 +147,9 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <div id="confirmDeleteModal" class="modal fade" tabindex="-1" role="dialog">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header bg-warning">
                     <h5 class="modal-title">Excluído</h5>
-                    <button type="button" class="btn-close btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn bg-light btn-close btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <p id="deleteMessage"></p>
@@ -169,9 +165,9 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header bg-danger text-light">
                     <h5 class="modal-title" id="logoutModalLabel">Sair do SysPD2</h5>
-                    <button type="button" class="btn-close btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn bg-light btn-close btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <p>Deseja sair do sistema?</p>
@@ -192,28 +188,39 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 
     <!-- Menu Principal Off-Canvas -->
     <div class="offcanvas offcanvas-end offcanvas-content" tabindex="-1" id="menuPrincipal" aria-labelledby="menuPrincipalLabel">
-        <div class="text-end mt-3 me-3">
-            <button class="btn btn-info btn-sm">Ajuda</button>
-        </div>
-        <div class="offcanvas-header">
-            <span class="fs-4 bi bi-menu-button-wide-fill"></span>
+        <div class="offcanvas-header bg-primary text-light">
+            <span class="fs-4 bi bi-menu-button-wide-fill me-2"></span>
             <h4 class="offcanvas-title">Menu SysPD2</h4>
-            <button type="button" class="btn-close btn-sm" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <button type="button" class="btn bg-light btn-close btn-sm" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-            <p>Aenean urna quam, finibus vitae sem eget, dictum tincidunt est.
+            <p class="pb-2">Aenean urna quam, finibus vitae sem eget, dictum tincidunt est.
                 Praesent suscipit ultricies justo, vitae tincidunt orci imperdiet sit amet.
                 Sed ultricies eros non massa rutrum, malesuada blandit odio dictum.
             </p>
             <div id="upload-controls" class="mt-2">
-                <button class="btn btn-warning btn-sm w-50" data-bs-toggle="modal" data-bs-target="#modalUpload">
+                <button class="btn btn-primary btn-sm w-50" data-bs-toggle="modal" data-bs-target="#modalUpload">
                     Enviar Arquivo PDF
                     <span class="bi bi-cloud-upload"></span>
                     <span class="bi bi-file-earmark-pdf"></span>
                 </button>
             </div>
             <div class="mt-2">
-                <button class="btn btn-danger btn-sm w-50" data-bs-toggle="modal" data-bs-target="#logoutModal">
+                <button class="btn btn-primary btn-sm px-3 w-50" type="button" data-bs-toggle="modal" data-bs-target="#modalSobre" aria-controls="modalSobre">
+                    Sobre
+                    <span class="bi bi-three-dots"></span>
+                </button>
+            </div>
+            <div class="mt-2">
+                <button class="btn btn-warning btn-sm px-3 w-50" type="button" data-bs-toggle="modal" data-bs-target="#modalAjuda" aria-controls="modalAjuda">
+                    Ajuda
+                    <span class="bi bi-patch-question"></span>
+                </button>
+            </div>
+        </div>
+        <div class="offcanvas-footer">
+            <div class="ms-3 mb-3">
+                <button class="btn btn-danger btn-sm px-3" data-bs-toggle="modal" data-bs-target="#logoutModal">
                     Sair do Sistema
                     <span class="bi bi-box-arrow-right"></span>
                 </button>
@@ -224,12 +231,12 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <!-- Modal Upload -->
     <div class="modal fade" id="modalUpload" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="modalUploadLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content text-center">
-                <div class="modal-header">
+            <div class="modal-content">
+                <div class="modal-header bg-primary text-light">
                     <h5 class="modal-title" id="logoutModalLabel">Enviar PD</h5>
-                    <button type="button" class="btn btn-close btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn bg-light btn-close btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body text-center">
                     <form action="upload.php" method="post" enctype="multipart/form-data">
                         <div class="form-group mb-3 mx-5 px-5">
                             <label for="pdf-date">Selecione a Data:</label>
@@ -241,7 +248,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
                                 required>
                         </div>
                         <div class="modal-footer">
-                            <button type="submit" class="btn btn-primary btn-sm">
+                            <button type="submit " class="btn btn-primary btn-sm">
                                 Upload
                                 <span class="bi bi-file-earmark-arrow-up"></span>
                             </button>
@@ -258,13 +265,13 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="messageModalLabel">Informação</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn bg-light btn-close btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <div id="modalMessage" class="alert"></div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Fechar</button>
+                    <button type="button" class="btn btn-primary btn-sm" data-bs-dismiss="modal">Fechar</button>
                 </div>
             </div>
         </div>
@@ -274,17 +281,114 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <div class="modal fade" id="modalSobre" tabindex="-1" aria-hidden="true" data-bs-backdrop="static">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header bg-primary text-light">
                     <h5 class="modal-title" id="modalSobreLabel">Sobre o sistema SysPD2</h5>
-                    <button type="button" class="btn-close btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn btn-close bg-light btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body pt-0">
+                <div class="modal-body">
                     <p class="fw-lighter text-center text-secondary">
                         Este sistema foi desenvolvido para facilitar o gerenciamento de arquivos PDF, utilizando Bootstrap 5 e as mais recentes tecnologias web. Ele permite o upload, visualização e exclusão de documentos de forma simples e segura, com navegação por meio de um calendário intuitivo. Além disso, conta com um sistema de autenticação para garantir que apenas usuários autorizados acessem funcionalidades críticas.
                     </p>
                 </div>
                 <div class="modal-footer pt-0">
-                    <p class="fw-bolder">Versão 2.0</p>
+                    <p class="fw-bolder">Versão alpha-1.0</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Ajuda -->
+    <div class="modal fade" id="modalAjuda" tabindex="-1" aria-hidden="true" data-bs-backdrop="static">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content">
+                <div class="modal-header bg-warning text-dark">
+                    <h5 class="modal-title" id="modalSobreLabel">Menu de ajuda do Sistema Plano do Dia 2</h5>
+                    <button type="button" class="btn btn-close bg-light btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mt-2">
+                        <h5>1. Administrando o Sistema</h5>
+                        <ol>
+                            <li>
+                                <strong>Login:</strong>
+                                <p>Na página inicial, clique no botão <em>Login</em>. Uma tela aparecerá solicitando suas credenciais.</p>
+                                <ul>
+                                    <li><strong>Usuário:</strong> Insira seu nome de usuário, conforme registrado pelo administrador.</li>
+                                    <li><strong>Senha:</strong> Digite a senha associada ao seu usuário.</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <strong>Acessar a Área Administrativa:</strong>
+                                <p>Após inserir as credenciais corretamente, você será redirecionado para a área administrativa, onde poderá gerenciar os arquivos PDF do Plano do Dia.</p>
+                            </li>
+                        </ol>
+                        <h3>2. Upload de Arquivos</h3>
+                        <ol>
+                            <li>
+                                <strong>Acesse a Seção de Upload:</strong>
+                                <p>No menu principal, clique no botão <em>Enviar Arquivo PDF</em>. Uma tela será exibida para que você selecione o arquivo.</p>
+                            </li>
+                            <li>
+                                <strong>Defina a data:</strong>
+                                <p>Clique no campo <em>Data</em> e escolha o dia a qual será associado o PD.</p>
+                            </li>
+                            <li>
+                                <strong>Escolha o Arquivo:</strong>
+                                <p>Clique no botão <em>Selecionar Arquivo</em> e navegue até o PDF desejado em seu computador.</p>
+                            </li>
+                            <li>
+                                <strong>Confirme o Upload:</strong>
+                                <p>Após selecionar o arquivo, clique no botão <em>Upload</em>. Uma mensagem de confirmação será exibida, indicando que o arquivo foi enviado com sucesso.</p>
+                            </li>
+                        </ol>
+                        <h3>3. Gerenciar Arquivos PDF</h3>
+                        <ol>
+                            <li>
+                                <strong>Visualizar PDFs Enviados:</strong>
+                                <p>Todos os PDFs serão listados ao lado de acordo com o mês exibido na tela. Clique no nome do arquivo para visualizar.</p>
+                            </li>
+                            <li>
+                                <strong>Excluir Arquivos:</strong>
+                                <p>Para excluir um arquivo:</p>
+                                <ul>
+                                    <li>Clique no ícone da lixeira ao lado do PDF.</li>
+                                    <li>Uma confirmação será exibida.</li>
+                                    <li>Após confirmar a exclusão, o arquivo será movido para a lixeira e não será mais visível na lista <em>Plano do Dia do Mês</em>.</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <strong>Restaurar Arquivos da Lixeira:</strong>
+                                <p>Se você deseja restaurar um arquivo excluído, vá até a pasta de <em>Lixeira</em> no menu e mova o arquivo de volta para a pasta de uploads.</p>
+                            </li>
+                        </ol>
+                        <h3>4. Ir para Data Específica</h3>
+                        <ol>
+                            <li>
+                                <strong>Selecione a Data:</strong>
+                                <p>Use o seletor de mês e ano na página de administração para escolher a data desejada.</p>
+                            </li>
+                            <li>
+                                <strong>Clique em Ir:</strong>
+                                <p>O sistema listará automaticamente os PDFs enviados naquele mês específico.</p>
+                            </li>
+                        </ol>
+                        <h3>5. Configuração de Conta</h3>
+                        <p>Você pode alterar as configurações da sua conta, como senha ou outras informações pessoais, acessando o menu <em>Configurações</em>, localizado no canto superior direito.</p>
+                        <h3>6. Efetuando Logout</h3>
+                        <ol>
+                            <li>
+                                <strong>Clique em Logout:</strong>
+                                <p>No canto superior direito, clique no botão <em>Logout</em>.</p>
+                            </li>
+                            <li>
+                                <strong>Confirmação:</strong>
+                                <p>Após clicar, você será redirecionado para a página inicial do sistema e precisará fazer login novamente para ter acesso à área administrativa.</p>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary btn-sm" data-bs-dismiss="modal">Fechar</button>
                 </div>
             </div>
         </div>
--- delete_pdf.php
+++ delete_pdf.php
@@ -7,9 +7,16 @@ $file = $_GET['file'] ?? '';
 
 $directory = "uploads/{$year}/" . str_pad($month, 2, '0', STR_PAD_LEFT);
 $filePath = "{$directory}/{$file}";
+$trashDir = ".trash/{$year}/" . str_pad($month, 2, '0', STR_PAD_LEFT);
+
+if (!is_dir($trashDir)) {
+    mkdir($trashDir, 0777, true);
+}
+
+$trashFilePath = "{$trashDir}/{$file}";
 
 if (file_exists($filePath)) {
-    if (unlink($filePath)) {
+    if (rename($filePath, $trashFilePath)) {
         echo json_encode(['status' => 'success']);
     } else {
         echo json_encode(['status' => 'error', 'message' => 'Erro ao excluir o arquivo.']);
